62506f2f77c800880077bea547a14ec3efd8fdb9
indiejoseph <indiejoseph@gmail.com>
2015-06-11 00:38:38 +0800

    Update

--- test/util.js
+++ test/util.js
@@ -1,15 +1,20 @@
 import * as util from '../word_representation/lib/util';
 import 'should';
 
-describe('util', function () {
+describe('util', () => {
 
 	describe('tokenizer', function() {
 
-		it('should tokenize a sentence', function () {
+		it('should tokenize a sentence', () => {
 			let s = 'I\'m feel good, Professor, 你好, Haven\'t';
 			let tokens = util.tokenize(s);
+			tokens.length.should.equal(7);
+		});
 
-			console.log(tokens);
+		it('should tokenize a CJK punctuations', () => {
+			let s = '、。〃〄々〆〇〈〉《》「」『』【】〒〓〔〕〖〗〘〙〚〛〜〝〞〟';
+			let tokens = util.tokenize(s);
+			tokens.length.should.equal(31);
 		});
 
 	});
--- word_representation/lib/util.js
+++ word_representation/lib/util.js
@@ -1,11 +1,11 @@
-import { XRegExp } from 'xregexp';
-import tokenizer from './tokenizer';
-import { EventEmitter } from 'events';
-
-// tokenizer.rule('punctuation', XRegExp("\\p{P}+"));
-tokenizer.rule('comma', /,/);
-
 export function tokenize (str) {
-	let tokens = tokenizer.tokenize(str);
+	let splitter = [
+		'([\u2000-\u206e])', // General punctuations
+		'([\u3000-\u303F])', //  CJK punctuations
+		'([a-zA-Z\p{L}\'\-]+)', // English word
+		'([\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d])' // CJK characters
+	].join('|');
+	let regex = new RegExp(splitter, 'g');
+	let tokens = str.match(regex);
 	return tokens;
 }
